f0a538b7195a2dfd27cb3125cc9565b9c20fd9f6
AadityaTripathi <tripathiaaditya02@gmail.com>
2023-08-30 21:25:34 +0530

    added basic structure

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" class="logo">CampusConnect</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div class="post">
+        <div class="image">
+          <img src="https://images.indianexpress.com/2023/08/pulse-.jpg?w=640" alt="" />
+        </div>
+        <div class="texts">
+          <h2>Seat-sharing may wait, INDIA to put in place groups, structure at Mumbai meeting</h2>
+          <p>28 parties to meet, up from 26 in Bengaluru; parties cool to the idea of a convenor, apprehensive about rocking political equations</p>
+        </div>
+      </div>
+      <div class="post">
+        <div class="image">
+          <img src="https://images.indianexpress.com/2023/08/pulse-.jpg?w=640" alt="" />
+        </div>
+        <div class="texts">
+          <h2>Seat-sharing may wait, INDIA to put in place groups, structure at Mumbai meeting</h2>
+          <p>28 parties to meet, up from 26 in Bengaluru; parties cool to the idea of a convenor, apprehensive about rocking political equations</p>
+        </div>
+      </div>
+      <div class="post">
+        <div class="image">
+          <img src="https://images.indianexpress.com/2023/08/pulse-.jpg?w=640" alt="" />
+        </div>
+        <div class="texts">
+          <h2>Seat-sharing may wait, INDIA to put in place groups, structure at Mumbai meeting</h2>
+          <p>28 parties to meet, up from 26 in Bengaluru; parties cool to the idea of a convenor, apprehensive about rocking political equations</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
